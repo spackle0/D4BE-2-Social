32c969a520b5e71d81badfe999851bb71a484fcb
spackle0 <spackle0@users.noreply.github.com>
2023-11-25 22:37:11 -0500

    password reset

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,23 +1,22 @@
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
 from .forms import LoginForm
 
-from django.contrib.auth.decorators import login_required
 
 @login_required
 def dashboard(request):
-    return render(request, 'account/dashboard.html', {"section": "dashboard"})
+    return render(request, "account/dashboard.html", {"section": "dashboard"})
+
 
 def user_login(request):
     if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(
-                request, username=cd["username"], password=cd["password"]
-            )
+            user = authenticate(request, username=cd["username"], password=cd["password"])
             if user is not None:
                 if user.is_active:
                     login(request, user)
