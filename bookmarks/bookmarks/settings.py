e8ead83710a7c46b0d9e4f7d343a463817b9852e
spackle0 <spackle0@users.noreply.github.com>
2023-11-28 00:10:51 -0500

    more image stuff

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "social_django",
     "django_extensions",
     "images.apps.ImagesConfig",
+    "easy_thumbnails",
 ]
 
 MIDDLEWARE = [
--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get("id")
+    action = request.POST.get("action")
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == "like":
+                image.users_like.add(request.user)
+            else:
+                image.user_like.remove(request.user)
+            return JsonResponse({"status": "ok"})
+        except Image.DoesNotExist:
+            pass
+    return JsonResponse({"status":"error"})
+
 from .forms import ImageCreateForm
+from .models import Image
+
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(
+        request, "images/image/detail.html", {"section": "images", "image": image}
+    )
 
 
 @login_required
@@ -22,5 +50,6 @@ def image_create(request):
     else:
         # build form with data provided by the bookmarlet via GET
         form = ImageCreateForm(data=request.GET)
-    return render(request, "images/images/create.html",
-                  {"section": "images", "form": form})
+    return render(
+        request, "images/image/create.html", {"section": "images", "form": form}
+    )
