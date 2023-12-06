0913e63699c40bc84d544b29d3cb72df986f2413
spackle0 <spackle0@users.noreply.github.com>
2023-12-05 22:38:20 -0500

    up to signals

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,3 +1,5 @@
+from actions.models import Action
+from actions.utils import create_action
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
@@ -20,6 +22,7 @@ def user_follow(request):
             user = User.objects.get(id=user_id)
             if action == "follow":
                 Contact.objects.get_or_create(user_from=request.user, user_to=user)
+                create_action(request.user, "is following", user)
             else:
                 Contact.objects.filter(user_from=request.user, user_to=user).delete()
             return JsonResponse({"status": "ok"})
@@ -80,6 +83,7 @@ def register(request):
             new_user.save()
             # Create the user profile
             Profile.objects.create(user=new_user)
+            create_action(new_user, "has created an account")
             return render(request, "account/register_done.html", {"new_user": new_user})
     else:
         user_form = UserRegistrationForm()
@@ -88,7 +92,19 @@ def register(request):
 
 @login_required
 def dashboard(request):
-    return render(request, "account/dashboard.html", {"section": "dashboard"})
+    # Display all actions by default
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list("id", flat=True)
+
+    if following_ids:
+        # If user is following others, retrieve only their actions
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions.select_related("user", "user__profile").prefetch_related("target")[
+        :10
+    ]
+    return render(
+        request, "account/dashboard.html", {"section": "dashboard", "actions": actions}
+    )
 
 
 def user_login(request):
